b1f83a0935f612edfcafbb861c39ccb1c00191f0
atharateeq01 <atharateeq01@gmail.com>
2024-05-10 16:32:47 +0500

    first commit

---
+++ app/components/ListCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddToCart from './AddToCart'
+
+const ListCard = () => {
+    return (
+        <div>
+            <h1>pro</h1>
+            <AddToCart />
+        </div >
+    )
+}
+
+export default ListCard
